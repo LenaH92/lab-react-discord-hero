58a7f59c1a5dcdeade791c2fabeb430ee12db4d9
Lena Cortes <lena.cortes@usal.es>
2024-12-10 15:33:20 +0100

    done 2.0

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,17 +12,21 @@ function App() {
           <img src={menuIcon} alt="menu icon" />
         </nav>
         <div>
-          <h1>IMAGINE A PLACE...</h1>
-          <p>...where you can belong to a school club, 
-            a gaming group, or a worldwide art 
-            community. 
-            Where just you and a handful of friends 
-            can spend time together. 
-            A place that makes it easy to talk every 
-            day and hang out more often.</p>
-          <button>Download for Mac</button>
-          <button>Open Discord in your browser</button>
-          <img src={discordBackground} alt="monsters hanging out" />
+          <div id="mainArea">
+            <h1 id='title'>IMAGINE A PLACE...</h1>
+            <p className='text'>
+              ...where you can belong to a school club, a gaming group, or a worldwide art 
+              community. 
+              Where just you and a handful of friends can spend time together. 
+              A place that makes it easy to talk every day and hang out more often.
+            </p>
+            <div id='buttonsDiv'>
+              <button id='macBTN' className='text'>Download for Mac</button>
+              <button id='openBrowserBTN' className='text'>Open Discord in your browser</button>
+            </div>
+          </div>
+          <div id='imgDiv'><img src={discordBackground} alt="monsters hanging out" id='backgroundIMG' /></div>
+          
         </div>
       </div>  
   );
